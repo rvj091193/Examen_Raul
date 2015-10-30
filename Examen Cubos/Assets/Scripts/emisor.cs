ac4b6b6901ff739140b5a8a8c669e8d8260c249c
Raul <raulvidana@hotmail.com>
2015-10-30 14:45:14 -0600

    Examen final, cubos en linea y colores magicos

---
+++ Examen Cubos/Assets/Scripts/emisor.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class emisor : MonoBehaviour {
+	public GameObject objetoPrefab;
+	
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		float pos;
+		//pos = Random.Range (-10, 10);
+		pos = Random.Range (-40, 40);
+		Vector3 newPos = new Vector3 (this.transform.position.x+pos,this.transform.position.y, this.transform.position.z);
+		Instantiate (objetoPrefab, newPos, Quaternion.identity);
+		
+	}
+}
